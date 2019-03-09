b0e1945a4b57fd647ea98ec39059b9899c005f71
Alexey Bulenkov <alex@bulenkov.com>
2019-03-09 03:38:40 +0300

    - refactoring fonts - load fonts from file - refactoring previous printer

--- src/nameBadgesPrinter/main.kt
+++ src/nameBadgesPrinter/main.kt
@@ -1,44 +1,30 @@
 package nameBadgesPrinter
 
+import java.io.File
 import java.util.*
+import kotlin.collections.ArrayList
 
-class FontSymbol(val symbol: Char, private val data: String, private val width: Int = 4) {
-    fun getSymbolLine(line: Int): String {
-        return this.data.slice((line - 1) * width until line * width)
-    }
-}
+class FontSymbol(val symbol: Char, val data: Array<String>)
 
-class Font(private val letterSpacing: Int = 1) {
+class Font(fontPath: String, spaceLength:Int) {
     private val symbols: ArrayList<FontSymbol> = ArrayList()
+    private var lineHeight:Int = 0
 
     init {
-        symbols.add(FontSymbol('A', "____|__||  |"))
-        symbols.add(FontSymbol('B', "___ |__]|__]"))
-        symbols.add(FontSymbol('C', "____|   |___"))
-        symbols.add(FontSymbol('D', "___ |  \\|__/"))
-        symbols.add(FontSymbol('E', "____|___|___"))
-        symbols.add(FontSymbol('F', "____|___|   "))
-        symbols.add(FontSymbol('G', "____| __|__]"))
-        symbols.add(FontSymbol('H', "_  _|__||  |"))
-        symbols.add(FontSymbol('I', "_||", 1))
-        symbols.add(FontSymbol('J', " _ |_|", 2))
-        symbols.add(FontSymbol('K', "_  _|_/ | \\_"))
-        symbols.add(FontSymbol('L', "_   |   |___"))
-        symbols.add(FontSymbol('M', "_  _|\\/||  |"))
-        symbols.add(FontSymbol('N', "_  _|\\ || \\|"))
-        symbols.add(FontSymbol('O', "____|  ||__|"))
-        symbols.add(FontSymbol('P', "___ |__]|   "))
-        symbols.add(FontSymbol('Q', "____|  ||_\\|"))
-        symbols.add(FontSymbol('R', "____|__/|  \\"))
-        symbols.add(FontSymbol('S', "____[__ ___]"))
-        symbols.add(FontSymbol('T', "___ |  | ", 3))
-        symbols.add(FontSymbol('U', "_  _|  ||__|"))
-        symbols.add(FontSymbol('V', "_  _|  | \\/ "))
-        symbols.add(FontSymbol('W', "_ _ _| | ||_|_|", 5))
-        symbols.add(FontSymbol('X', "_  _ \\/ _/\\_"))
-        symbols.add(FontSymbol('Y', "_   _ \\_/   |  ", 5))
-        symbols.add(FontSymbol('Z', "___   /  /__"))
-        symbols.add(FontSymbol(' ', "            "))
+        val reader = File(fontPath).inputStream().bufferedReader()
+        val (lineHeight: Int, charCount: Int) = reader.readLine().split(" ").map { it.toInt() }
+
+        for (i in 0 until charCount) {
+            val fontChar = reader.readLine()[0]
+            val charData = Array<String>(lineHeight) { reader.readLine() }
+
+            this.symbols.add(FontSymbol(fontChar, charData))
+        }
+
+        val spaceData = Array<String>(lineHeight) { " ".repeat(spaceLength) }
+
+        this.symbols.add(FontSymbol(' ', spaceData))
+        this.lineHeight = lineHeight
     }
 
     private fun getSymbol(symbol: Char): FontSymbol? {
@@ -46,17 +32,13 @@ class Font(private val letterSpacing: Int = 1) {
     }
 
     fun getTextLines(text: String): Array<String> {
-        val lines = Array(3) { String() }
+        val lines = Array<String>(this.lineHeight) { "" }
 
-        for (r in 0 until 3) {
+        for (l in 0 until this.lineHeight) {
             for (i in 0 until text.length) {
-                val symbol = this.getSymbol(text.toUpperCase()[i]) ?: continue
+                val symbol = this.getSymbol(text[i]) ?: continue
 
-                if (i != 0) {
-                    lines[r] += " ".repeat(this.letterSpacing)
-                }
-
-                lines[r] += symbol.getSymbolLine(r + 1)
+                lines[l] += symbol.data[l]
             }
         }
 
@@ -64,7 +46,7 @@ class Font(private val letterSpacing: Int = 1) {
     }
 }
 
-fun readline(info:String): String {
+fun readline(info: String): String {
     val scanner = Scanner(System.`in`)
     print("$info: ")
 
@@ -74,22 +56,46 @@ fun readline(info:String): String {
     return name
 }
 
-fun printCard(name:String, status:String) {
-    val font = Font()
-    val lines = font.getTextLines(name)
-    val contentLength = lines[0].length
-    val leftPadding = (contentLength - status.length) / 2
-    val rightPadding = contentLength - status.length - leftPadding
-    val divider = "*".repeat(contentLength + 6)
+fun alignCentre(badgeData: ArrayList<String>): ArrayList<String> {
+    val maxSize:Int = badgeData.map{ it.length }.max() ?: 0
 
-    println(divider)
+    return ArrayList<String>(badgeData.map {
+        if (it.length < maxSize) {
+            val leftPadding = (maxSize - it.length) / 2
+            val rightPadding = maxSize - it.length - leftPadding
 
-    for (i in 0 until lines.size) {
-        println("*  ${lines[i]}  *")
-    }
+            return@map "${" ".repeat(leftPadding)}$it${" ".repeat(rightPadding)}"
+        }
 
-    println("*  ${" ".repeat(leftPadding)}$status${" ".repeat(rightPadding)}  *")
-    println(divider)
+        return@map it
+    })
+}
+
+fun printCard(name: String, status: String) {
+    val romanFont = Font("fonts/roman.txt", 10)
+    val mediumFont = Font("fonts/medium.txt", 5)
+    var badgeData = ArrayList<String>()
+
+    badgeData.addAll(romanFont.getTextLines(name))
+    badgeData.addAll(mediumFont.getTextLines(status))
+
+    badgeData = alignCentre(badgeData)
+    badgeData = addBorder(badgeData)
+
+    badgeData.forEach { println(it) }
+}
+
+fun addBorder(badgeData: ArrayList<String>): ArrayList<String> {
+    val borderedBadgeData = ArrayList<String>()
+    val borderLength = badgeData.first().length + 8
+
+    borderedBadgeData.add("8".repeat(borderLength))
+    borderedBadgeData.addAll(badgeData.map {
+        return@map "88  $it  88"
+    })
+    borderedBadgeData.add("8".repeat(borderLength))
+
+    return borderedBadgeData
 }
 
 fun main() {
@@ -98,4 +104,3 @@ fun main() {
 
     printCard(name, status)
 }
-
